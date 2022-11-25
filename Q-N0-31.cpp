bdd65a5d50f6b20d607f5cb2f29384e3fbc9eb9e
Nadeemmuktar12345 <93705958+Nadeemmuktar12345@users.noreply.github.com>
2022-11-25 23:05:49 +0500

    Add files via upload

---
+++ Q-N0-31.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int weight;
+	
+   If ( a.weight > b.weight )
+		return true;
+	else if ( a.weight < b.weight )
+		return false;
+	else
+	{
+		if ( a.name < b.name)
+			return true;
+		else if( a.name > b.name )
+			return false;
+		else
+		{
+			if( a.age < b.age)
+				return true;
+			else if( a.age > b.age )
+					return false;
+			else
+				return true;
+		}
+   }
+   else
+   cout<<"Not valid";
+} 
